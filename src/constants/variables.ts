12a5eae6dc91ab85a13aaa93fd3f77d1497c79ab
Jiayi Wang <jiayi.wang@student.uni-luebeck.de>
2026-02-22 23:51:36 +0100

    update endpoint path

--- src/constants/variables.ts
+++ src/constants/variables.ts
@@ -4,4 +4,4 @@ export const ITINERARY_DEFAULT_IMAGE = require('@/assets/images/fallback.jpg');
 
 // Image storage
 export const IMAGE_BUCKET_PATH = 'v1/ui/';
-export const IMAGE_WORKER_BASE_URL = 'https://worker.echoyutian.com';
+export const IMAGE_WORKER_BASE_URL = 'https://worker.echoyutian.com/secure/';
